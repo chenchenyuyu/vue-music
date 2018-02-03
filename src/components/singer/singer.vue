a559f6ebadb27298c565796d0ec26c0107d4ac9d
chenyu <chenyu02@corp.netease.com>
2018-02-03 19:36:26 +0800

    commit singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,35 +1,112 @@
 <template>
-  <div class="listview">
-    <div class="list-group">
-      <h2 class="list-fixed">热门</h2>
-      <ul class="list-song">
-         <li v-for="(item, index) in items" :key="index" class="list-group-item">
-           <img src="" alt="avatar" class="avatar">
-           <span class="name">名字</span>
-           {{item.song}}
-         </li>
-      </ul>
-    </div>
+  <div class="singer" ref="singer">
+    <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// 模拟数据
+import ListView from 'base/listview/listview'
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+import { mapMutations } from 'vuex'
+import { playlistMixin } from 'common/js/mixin'
+
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
+
 export default {
+  mixins: [playlistMixin],
   data() {
     return {
-      items: [{ song: '青花瓷' }, { song: '勇气' }]
+      singers: []
     }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    handlePlaylist(playlist) {
+      const bottom = playlist.length > 0 ? '60px' : ''
+      this.$refs.singer.style.bottom = bottom
+      this.$refs.list.refresh()
+    },
+    selectSinger(singer) {
+      this.$router.push({
+        path: `/singer/${singer.id}`
+      })
+      this.setSinger(singer)
+    },
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          console.log('singerList', res.data);
+          this.singers = this._normalizeSinger(res.data.list)
+        }
+      })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(
+            new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            })
+          )
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(
+          new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          })
+        )
+      })
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    },
+    ...mapMutations({
+      setSinger: 'SET_SINGER'
+    })
+  },
+  components: {
+    ListView
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-.list-fixed
-    height: 30px
-    line-height: 30px
-    padding-left: 20px
-    font-size: 12px
-    color: rgba(255,255,255,0.5)
-    background: #333
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
 </style>
