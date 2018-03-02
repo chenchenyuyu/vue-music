b0b0d0bf6c26339daddd231a31cfd9532987d4c5
chenyu <chenyu02@corp.netease.com>
2018-03-02 14:29:11 +0800

    commit music-list

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -20,3 +20,35 @@ export function getData(el, name, val) {
   }
   return el.setAttribute(prefix + name)
 }
+
+// 为元素属性添加前缀，实现兼容
+
+let elementStyle = document.createElement('div').style
+
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    strandard: 'transform'
+  }
+
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+  if (vendor === 'standard') {
+    return style
+  }
+
+  return vendor + style.charAt(0).toUpperCase + style.substr(1)
+}
