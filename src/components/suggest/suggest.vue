72060fc9b9741e1bbe7c3c1da6f0b76b9cec4447
chenyu <chenyu02@corp.netease.com>
2018-03-06 22:04:28 +0800

    modify suggest

--- src/components/suggest/suggest.vue
+++ src/components/suggest/suggest.vue
@@ -19,11 +19,15 @@
 import Scroll from 'base/scroll/scroll'
 import Loading from 'base/loading/loading'
 import NoResult from 'base/no-result/no-result'
+import { search } from 'api/search'
+import { ERR_OK } from 'api/config'
+import { createSong } from 'common/js/song'
+import { mapMutations, mapActions } from 'vuex'
+import Singer from 'common/js/singer'
+
 export default {
   data() {
-     return {
-       
-     }
+    return {}
   },
   components: {
     Scroll,
@@ -33,34 +37,49 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
- @import "~common/stylus/variable"
-  @import "~common/stylus/mixin"
+@import '~common/stylus/variable';
+@import '~common/stylus/mixin';
 
-  .suggest
-    height: 100%
-    overflow: hidden
-    .suggest-list
-      padding: 0 30px
-      .suggest-item
-        display: flex
-        align-items: center
-        padding-bottom: 20px
-      .icon
-        flex: 0 0 30px
-        width: 30px
-        [class^="icon-"]
-          font-size: 14px
-          color: $color-text-d
-      .name
-        flex: 1
-        font-size: $font-size-medium
-        color: $color-text-d
-        overflow: hidden
-        .text
-          no-wrap()
-    .no-result-wrapper
-      position: absolute
-      width: 100%
-      top: 50%
-      transform: translateY(-50%)
+.suggest {
+  height: 100%;
+  overflow: hidden;
+
+  .suggest-list {
+    padding: 0 30px;
+
+    .suggest-item {
+      display: flex;
+      align-items: center;
+      padding-bottom: 20px;
+    }
+
+    .icon {
+      flex: 0 0 30px;
+      width: 30px;
+
+      [class^='icon-'] {
+        font-size: 14px;
+        color: $color-text-d;
+      }
+    }
+
+    .name {
+      flex: 1;
+      font-size: $font-size-medium;
+      color: $color-text-d;
+      overflow: hidden;
+
+      .text {
+        no-wrap();
+      }
+    }
+  }
+
+  .no-result-wrapper {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
 </style>
